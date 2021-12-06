3c3708fc2d93ef885e560ea92705edc28e8688d2
Mutuyimana Alice_219003365 <amutuyimana64@gmail.com>
2021-12-06 14:15:33 +0200

    procfile file added

--- restaurant_API/settings.py
+++ restaurant_API/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,3 +142,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10
 }
+
+django_heroku.settings(locals())
